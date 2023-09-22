7836246cc985901703c459b55c2b219810fdad16
cristian camacho <cristianduvang12@hotmail.com>
2023-09-21 20:44:20 -0500

    modificación de unicaciones e imprimirContactos

--- project2.js
+++ project2.js
@@ -1,14 +1,14 @@
 // Crear una lista de contactos
 // Crear una lista de contactos
 let listaDeContactos = [
-    { id: 1, nombres: "Juan Camilo", apellidos: "Camacho Barreto", telefono: "3156481684", ubicaciones: ['Bogota, carrera 8A']},
-    { id: 2, nombres: "Maria Camila", apellidos: "Lopez Treviño", telefono: "2165814684", ubicaciones: ['cali, calle 14']},
-    { id: 3, nombres: "Samuel", apellidos: "Paez Granja", telefono: "31897981465", ubicaciones: ['Medellin, Bello calle 19']},
+    { id: 1, nombres: "Juan Camilo", apellidos: "Camacho Barreto", telefono: "3156481684", ubicaciones: [{ ciudad: 'Bogota', direccion: 'carrera 8A' }]},
+    { id: 2, nombres: "Maria Camila", apellidos: "Lopez Treviño", telefono: "2165814684", ubicaciones: [{ ciudad: 'Cali', direccion: 'calle 14' }]},
+    { id: 3, nombres: "Samuel", apellidos: "Paez Granja", telefono: "31897981465", ubicaciones: [{ ciudad: 'Medellin', direccion: 'Bello calle 19' }]},
   ];
   
   // Función para añadir un nuevo contacto
-  function agregarContacto(id, nombres, apellidos, telefono, ubicaciones) {
-    listaDeContactos.push({ id, nombres, apellidos, telefono, ubicaciones });
+  function agregarContacto(id, nombres, apellidos, telefono, ciudad, direccion) {
+    listaDeContactos.push({ id, nombres, apellidos, telefono, ubicaciones: [{ ciudad, direccion }] });
   }
 
   function borrarContacto(id) {
@@ -25,11 +25,17 @@ let listaDeContactos = [
   function imprimirContactos() {
     console.log("Lista de Contactos:");
     listaDeContactos.forEach(function(contacto) {
-      console.log(contacto.id + ". "  + contacto.nombres + " " + contacto.apellidos + " " + contacto.telefono + " " + contacto.ubicaciones);
+      console.log(contacto.id + ". "  + contacto.nombres + " " + contacto.apellidos + " " + contacto.telefono);
+      contacto.ubicaciones.forEach(function (ubicacion) {
+        console.log("   - Ciudad: " + ubicacion.ciudad + ", Dirección: " + ubicacion.direccion);
+      });
     });
   }
 
 
+imprimirContactos();
+
+agregarContacto(4, "Ana", "Gomez", "3159876543", "Barranquilla", "Avenida 123");
 imprimirContactos();
 
 borrarContacto(1);
